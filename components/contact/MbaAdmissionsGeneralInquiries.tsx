9edda312dc397598184002dd24e73e763776fd8e
Thirunavukarasan8489 <thirugopi733@gmail.com>
2026-02-12 16:42:05 +0530

    contact page content updated

--- components/contact/MbaAdmissionsGeneralInquiries.tsx
+++ components/contact/MbaAdmissionsGeneralInquiries.tsx
@@ -14,7 +14,7 @@ export default function MbaAdmissionsGeneralInquiries() {
                     </p> */}
                 </div>
 
-                <div className="space-y-6 md:space-y-8">
+                <div className="space-y-6 md:space-y-8 mb-6">
                     <div className="">
                         <h3 className="text-xl md:text-2xl font-trade-gothic-bold text-booth-dark-gray mb-2">
                             RVS College of Arts & Science
@@ -33,6 +33,33 @@ export default function MbaAdmissionsGeneralInquiries() {
                             Coimbatore, Tamil Nadu – 641 402
                         </p>
                     </div>
+                    {/* <div className='flex items-baseline justify-start gap-1.5'>
+                        <h3 className="text-xl md:text-2xl font-trade-gothic-bold text-booth-dark-gray mb-2">
+                            Email:
+                        </h3>
+                        <a href="mailto:admissions@rvscasmba.ac.in" className="text-lg hover:text-booth-maroon hover:underline transition-all duration-300 md:text-xl text-booth-dark-gray font-trade-gothic-light">
+                            admissions@rvscasmba.ac.in
+                        </a>
+                    </div>
+                    <div className='flex items-baseline justify-start gap-1.5'>
+                        <h3 className="text-xl md:text-2xl font-trade-gothic-bold text-booth-dark-gray mb-2">
+                            Phone:
+                        </h3>
+                        <a href="tel:+919876543210" className="text-lg hover:text-booth-maroon hover:underline transition-all duration-300 md:text-xl text-booth-dark-gray font-trade-gothic-light">
+                            +91 9876543210
+                        </a>
+                    </div>
+                    <div className='flex items-baseline justify-start gap-1.5'>
+                        <h3 className="text-xl md:text-2xl font-trade-gothic-bold text-booth-dark-gray mb-2">
+                            Office Hours:
+                        </h3>
+                        <p className="text-lg md:text-xl text-booth-dark-gray font-trade-gothic-light">
+                            Mon – Sat, 9:30 AM to 5:30 PM
+                        </p>
+                    </div> */}
+                </div>
+
+                <div>
                     <div className='flex items-baseline justify-start gap-1.5'>
                         <h3 className="text-xl md:text-2xl font-trade-gothic-bold text-booth-dark-gray mb-2">
                             Email:
