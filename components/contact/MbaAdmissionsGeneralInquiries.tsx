bbb5fe0da1dfa9ab74c114f87b2a10ccd960759b
Thirunavukarasan8489 <thirugopi733@gmail.com>
2026-02-12 16:46:32 +0530

    conta

--- components/contact/MbaAdmissionsGeneralInquiries.tsx
+++ components/contact/MbaAdmissionsGeneralInquiries.tsx
@@ -64,16 +64,16 @@ export default function MbaAdmissionsGeneralInquiries() {
                         <h3 className="text-xl md:text-2xl font-trade-gothic-bold text-booth-dark-gray mb-2">
                             Email:
                         </h3>
-                        <a href="mailto:admissions@rvscasmba.ac.in" className="text-lg hover:text-booth-maroon hover:underline transition-all duration-300 md:text-xl text-booth-dark-gray font-trade-gothic-light">
-                            admissions@rvscasmba.ac.in
+                        <a href="mailto:enroll@rvsgroup.com" className="text-lg hover:text-booth-maroon hover:underline transition-all duration-300 md:text-xl text-booth-dark-gray font-trade-gothic-light">
+                            enroll@rvsgroup.com
                         </a>
                     </div>
                     <div className='flex items-baseline justify-start gap-1.5'>
                         <h3 className="text-xl md:text-2xl font-trade-gothic-bold text-booth-dark-gray mb-2">
                             Phone:
                         </h3>
-                        <a href="tel:+919876543210" className="text-lg hover:text-booth-maroon hover:underline transition-all duration-300 md:text-xl text-booth-dark-gray font-trade-gothic-light">
-                            +91 9876543210
+                        <a href="tel:+918883322433" className="text-lg hover:text-booth-maroon hover:underline transition-all duration-300 md:text-xl text-booth-dark-gray font-trade-gothic-light">
+                            +91 88833 22433
                         </a>
                     </div>
                     <div className='flex items-baseline justify-start gap-1.5'>
