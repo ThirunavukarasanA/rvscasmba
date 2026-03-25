c417851abc568ea1995335aa9f4adea7db60edb5
Thirunavukarasan8489 <thirugopi733@gmail.com>
2026-03-25 10:33:03 +0530

    events image shown bug finished

--- components/events/eventsData.tsx
+++ components/events/eventsData.tsx
@@ -6,6 +6,7 @@ export const pastEvents = [
         title: "Personality Development Programme – I MBA Students",
         description: "The objective of this programme is to build self-confidence, enhance self-esteem and improve overall personality of the students.",
         image: "/images/Social/personality-development-programme.webp",
+        slugimage: "/images/Social/personality-development-programme.png",
         slugtitle: "The objective of this programme is to build self-confidence, enhance self-esteem and improve overall personality of the students.",
         resource: "Resource Person: Mr.Jayaraman Umashankar,Vibha Training Academy,Salem,",
         date: "Date: 23/01/2020",
@@ -17,6 +18,7 @@ export const pastEvents = [
         title: "Reunion 2019",
         description: "RVS CAS MBA is happy to host reunion for the Alumni batch 2024-2025. It gives us immense pleasure to continue everlasting ties with you.",
         image: "/images/Social/reunion.webp",
+        slugimage: "/images/Social/reunion.png",
         slugtitle: "Reunion 2019 (Alumni meet)",
         slugpara: "RVS CAS MBA is happy to host reunion for the Alumni batch 2001-03 and 2002-04. It gives us immense pleasure to continue everlasting ties with you. Hope you are faring well in your job and earning laurels for yourself as well as for your family and the Institution. We are sure that you will certainly mark this day in your calendar to walk down the memory lanes and make it available to be with your adorable teachers, mentors and not the least, your bosom friends and colleagues. We welcome all the Alumni of these batches to honor the occasion and make it a success.",
         resource: "",
@@ -27,6 +29,7 @@ export const pastEvents = [
         title: "Machine Learning",
         description: "Resource Person: Sathya Narayanan, Associate Director, Cognizant Coimbatore",
         image: "/images/Social/machine-learning.webp",
+        slugimage: "/images/Social/machine-learning.png",
         slugtitle: "Machine Learning",
         resource: "Resource Person: Sathya Narayanan, Associate Director, Cognizant Coimbatore",
         date: "Date: 01.11.2021",
@@ -256,7 +259,7 @@ export const featuredEvents = [
         title: "The head hunters 3.0 – HR FEST 2025",
         description: "Presented by RVS CAS MBA Human Resource, RVS College of Arts and Science",
         image: "/images/Social/HR-FEST.webp",
-        slugimage: "",
+        slugimage: "/images/Social/HR-FEST.png",
         link: "#"
     },
     {
@@ -264,32 +267,41 @@ export const featuredEvents = [
         title: "Six-Day Faculty Development Programme",
         description: "The School of Management Studies-PG (MBA) successfully hosted a six-day Faculty Development Programme (FDP)",
         image: "/images/Social/FDP.webp",
-        slugimage: "",
+        slugimage: "/images/Social/FDP.png",
         link: "#"
     }
 ];
 
 export const upcomingEvents = [
     {
+        id: 1,
         title: "Onam Celebration 2025",
         description:
             "The School of Management Studies (PG) celebrated Onam 2025.",
         image:
             "/images/Social/onam.webp",
+        slugimage: "/images/Social/onam.png",
+        link: "#"
     },
     {
+        id: 2,
         title: "Vidhyarambha 2025 is here!",
         description:
             "RVS College of Arts and Science is all set to welcome the incoming MBA batch with VIDYARAMBHA 2025 — an inspiring and engaging orientation program scheduled from 15th July to 21st July 2025.",
         image:
             "/images/Social/Vidyarambha_2025.webp",
+        slugimage: "/images/Social/Vidyarambha_2025.png",
+        link: "#"
     },
     {
+        id: 3,
         title: "Campus to Corporate - Bridge Program",
         description:
             "RVS College of Arts & Science (RVS CAS MBA) successfully organized the 'Campus to Corporate - Bridge Program' on 01 Aug 2024, at 9:30 am in the prestigious VM Hall.",
         image:
             "/images/Social/campus-to-corporate.webp",
+        slugimage: "/images/Social/campus-to-corporate.png",
+        link: "#"
     },
 ];
 
